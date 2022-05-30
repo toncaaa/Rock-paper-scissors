a8d57f39667fe34edc3b11c32b405e3d8c644ea0
toncaaa <antoniotoncaraic@gmail.com>
2022-05-30 13:45:35 +0200

    Edit ComputerPlay to generate random choice, add conditionals to PlayRound function

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
+
 function computerPlay() {
-    let play = Math.random();
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    const play = choice[Math.floor(Math.random()*choice.length)];
+    console.log(choice.length);
+    console.log(Math.random()); // generates a number between 0 and 1
     console.log(play);
-    if (play <= 0.33) {
-        play = 'Rock'; 
-    }
-    else if (play > 0.33 && play < 0.66){
-        play = 'Paper';
-    }
-    else {
-        play = 'Scissors';
-    }
-    console.log(play);
+    return play;
 }
-computerPlay();
+const computerSelection = computerPlay(); // assign value of function to a variable
+let playerSelection = prompt('Enter your choice: ');
+
+console.log(playerSelection);
+
+
+function playRound (playerSelection, computerSelection) {  //play one round
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase(); // making input case insensitive 
+     if (playerSelection === computerSelection) {   // game rules 
+        console.log('It\s a draw');
+    }
+    else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        }
+    else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        }
+    else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+    }
+    else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        }
+    else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection));
 
