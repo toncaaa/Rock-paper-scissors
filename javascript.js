834053a10761a53e5090eebd5311d6e40d6ee9f9
toncaaa <antoniotoncaraic@gmail.com>
2022-05-31 09:50:40 +0200

    Add comments

--- javascript.js
+++ javascript.js
@@ -1,21 +1,17 @@
 
 function computerPlay() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    const play = choice[Math.floor(Math.random()*choice.length)];
-    console.log(choice.length);
-    console.log(Math.random()); // generates a number between 0 and 1
+    const choice = ['Rock', 'Paper', 'Scissors']; // declare game choices   
+    const play = choice[Math.floor(Math.random()*choice.length)]; // computer's random play 
     console.log(play);
     return play;
 }
-const computerSelection = computerPlay(); // assign value of function to a variable
-let playerSelection = prompt('Enter your choice: ');
-
+const computerSelection = computerPlay(); // assign value of a function to a variable
+let playerSelection = prompt('Enter your choice (Rock, Paper or Scissors): '); // prompting user for their pick 
 console.log(playerSelection);
-
 
 function playRound (playerSelection, computerSelection) {  //play one round
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase(); // making input case insensitive 
-     if (playerSelection === computerSelection) {   // game rules 
+     if (playerSelection === computerSelection) {   // game conditionals
         console.log('It\s a draw');
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
