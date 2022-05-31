1ee596d90c34f6d993d2ef8fadbd6048003f68b8
toncaaa <antoniotoncaraic@gmail.com>
2022-05-31 10:06:52 +0200

    Add return variable to function playRound

--- javascript.js
+++ javascript.js
@@ -11,27 +11,29 @@ console.log(playerSelection);
 
 function playRound (playerSelection, computerSelection) {  //play one round
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase(); // making input case insensitive 
+    let result;
      if (playerSelection === computerSelection) {   // game conditionals
-        console.log('It\s a draw');
+        result = 'It\s a draw';
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        result = `You've won ${playerSelection} beats ${computerSelection}`;
         }
     else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
-        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+        result = `You've lost ${computerSelection} beats ${playerSelection}`;
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        result = `You've won ${playerSelection} beats ${computerSelection}`;
         }
     else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
-        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+        result = `You've lost ${computerSelection} beat ${playerSelection}`;
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        console.log(`You've won ${playerSelection} beats ${computerSelection}`);
+        result = `You've won ${playerSelection} beat ${computerSelection}`;
         }
     else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
-        console.log(`You've lost ${computerSelection} beats ${playerSelection}`);
+        result = `You've lost ${computerSelection} beats ${playerSelection}`;
     }
+    return result; 
 }
 
 console.log(playRound(playerSelection,computerSelection));
